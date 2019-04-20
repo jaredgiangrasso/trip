4fd6f3191684f27241a1d5d0cbb85785361d2e92
Jared Giangrasso <jagiangrasso@gmail.com>
2019-04-20 13:32:42 -0400

    github pages commit

---
+++ public/js/smooth-scroll.js
@@ -0,0 +1,4 @@
+var scroll = new SmoothScroll('a[href*="#lorem"]', {speed: 1000, easing: 'easeInOutQuad'});
+var scroll = new SmoothScroll('a[href*="#ipsum"]', {easing: 'easeInOutQuad'});
+var scroll = new SmoothScroll('a[href*="#dolor"]', {easing: 'easeInOutQuad'});
+var scroll = new SmoothScroll('a[href*="#form"]', {easing: 'easeInOutQuad'});
