fc7d19870c8cb4960b72cecc5b45b5bcb3b873ff
Jared Giangrasso <jagiangrasso@gmail.com>
2019-04-21 12:54:47 -0400

    update styles

--- public/index.js
+++ public/index.js
@@ -3,9 +3,9 @@
 // Get a reference to the <path>
 let path = document.querySelector('#path');
 // Get a reference to content areas
-let card1 = document.querySelector('.card1');
-let card2 = document.querySelector('.card2');
-let card3 = document.querySelector('.card3');
+let loremPic = document.querySelector('#loremPic');
+let ipsumPic = document.querySelector('#ipsumPic');
+let dolorPic = document.querySelector('#dolorPic');
 // Get length of path... ~577px in this demo
 let pathLength = path.getTotalLength();
 
@@ -14,16 +14,16 @@ path.style.strokeDasharray = pathLength + ' ' + pathLength;
 path.style.strokeDashoffset = pathLength;
 
 updatePath = () => {
-	let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop)*.8 / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+	let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop)*.9 / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
 	let drawLength = pathLength * scrollPercentage;
 	path.style.strokeDashoffset = pathLength - drawLength;
 
-	let card1Pos = 4 + scrollPercentage * 15;
-	card1.style.top = card1Pos + '%';
-	let card2Pos = 30 + scrollPercentage * 15;
-	card2.style.top = card2Pos + '%';
-	let card3Pos = 53 + scrollPercentage * 15;
-	card3.style.top = card3Pos + '%';
+	let loremPicPos = scrollPercentage * 10 + 2;
+	loremPic.style.top = loremPicPos + '%';
+	let ipsumPicPos = scrollPercentage * 100 - 50;
+	ipsumPic.style.top = ipsumPicPos + '%';
+	let dolorPicPos = scrollPercentage * 100 - 100;
+	dolorPic.style.top = dolorPicPos + '%';
 }
 
 window.addEventListener('scroll', updatePath);
